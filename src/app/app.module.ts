a7bf609f1d360ae4a103b2aed95c41dea611cc2f
Loc Nguyen <vinhloc1996@yahoo.com.vn>
2022-10-22 23:14:46 +0700

    Generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { CheckoutComponent } from './components/form/checkout/checkout.component';
+import { DetailComponent } from './components/product/detail/detail.component';
+import { ListComponent } from './components/product/list/list.component';
+import { ListItemComponent } from './components/product/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    NavbarComponent,
+    CheckoutComponent,
+    DetailComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
